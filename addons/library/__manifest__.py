cb7107759af4ed69cf3ee7a8499856c92c99bb6d
“shahdNA” <shahd.alnabelsi1996@gmail.com>
2021-12-30 06:10:43 -0500

    first push

---
+++ addons/library/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "library_mangement",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "my library",
+    'category': 'sales',
+    'description': "",
+    'data': [
+        'views/library_view.xml',
+
+    ],
+    'demo': [
+        'demo/demo_library_data.xml',
+    ],
+
+
+
+}
